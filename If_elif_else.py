2369dfa708270f8a85556e7e393ba4df5e9420a4
Sandesh Kumar <55488045+sandeshjonnala@users.noreply.github.com>
2019-12-18 22:31:31 +0530

    Create If_elif_else.py
    Check whis is greater value a or b

---
+++ If_elif_else.py
@@ -0,0 +1,9 @@
+a = input('Enter 1st value:')
+b = input('Enter 2nd value:')
+
+if b > a:
+  print(b+"is greater than"+a)
+elif a == b:
+  print(a+" and "+b+" are equal")
+else:
+  print(a+"is greater than"+b)
